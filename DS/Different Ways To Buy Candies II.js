8aefa8098e700449905757148debe8f76ff5444c
vasukomuravelli <vasukomuravelli96@gmail.com>
2022-02-10 16:50:30 +0530

    helllo

--- DS/Different Ways To Buy Candies II.js
+++ DS/Different Ways To Buy Candies II.js
@@ -1,13 +1,27 @@
-// Link : https://oj.masaischool.com/contest/3046/problem/01
+// Link : https://oj.masaischool.com/contest/3046/problem/04
 function runProgram(input) {
   input = input.split("\n");
   let [N, K] = input[0].trim().split(" ").map(Number);
   let arr = input[1].trim().split(" ").map(Number);
-  let first = arr.slice(0, N);
-  let second = arr.slice(N, 2 * N);
-  let sum1 = first.reduce((acc, cur) => acc + cur);
-  let sum2 = second.reduce((acc, cur) => acc + cur);
-  console.log(Math.abs(sum1 - sum2) > K ? "Invalid" : "Valid");
+  Candies(N, K, arr, [], 0, 0);
+  if (!flag) {
+    console.log(-1);
+    return;
+  }
+}
+let obj = {};
+let flag = false;
+function Candies(N, K, arr, ans, curr, sum) {
+  if (sum == K && !obj[ans.join(" ")]) {
+    obj[ans.join(" ")] = 1;
+    flag = true;
+    console.log(ans.join(" "));
+  }
+  if (sum > K || curr >= N) {
+    return;
+  }
+  Candies(N, K, arr, [...ans, arr[curr]], curr + 1, sum + arr[curr]);
+  Candies(N, K, arr, ans, curr + 1, sum);
 }
 if (process.env.USERNAME === "vasuk") {
   runProgram(`5`);
--- DS/Different Ways To Buy Candies.js
+++ DS/Different Ways To Buy Candies.js
@@ -1,13 +1,26 @@
-// Link : https://oj.masaischool.com/contest/3046/problem/01
+// Link : https://oj.masaischool.com/contest/3046/problem/03
 function runProgram(input) {
   input = input.split("\n");
   let [N, K] = input[0].trim().split(" ").map(Number);
   let arr = input[1].trim().split(" ").map(Number);
-  let first = arr.slice(0, N);
-  let second = arr.slice(N, 2 * N);
-  let sum1 = first.reduce((acc, cur) => acc + cur);
-  let sum2 = second.reduce((acc, cur) => acc + cur);
-  console.log(Math.abs(sum1 - sum2) > K ? "Invalid" : "Valid");
+  Candies(N, K, arr, [], 0, 0);
+  if (!flag) {
+    console.log(-1);
+    return;
+  }
+}
+let flag = false;
+function Candies(N, K, arr, ans, curr, sum) {
+  if (sum == K) {
+    flag = true;
+    console.log(ans.join(" "));
+  }
+  if (sum > K || curr >= N) {
+    return;
+  }
+  for (let i = curr; i < N; i++) {
+    Candies(N, K, arr, [...ans, arr[i]], i, sum + arr[i]);
+  }
 }
 if (process.env.USERNAME === "vasuk") {
   runProgram(`5`);
