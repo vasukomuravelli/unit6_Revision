b972e8c79856adfbfe9952fd440366e1668118d5
vasukomuravelli <vasukomuravelli96@gmail.com>
2022-02-11 21:28:19 +0530

    hey ds

---
+++ DS/SelectionSort.js
@@ -0,0 +1,39 @@
+function runProgram(input) {
+  input = input.split("\n");
+  let N = +input[0];
+  let arr = input[1].trim().split(" ").map(Number);
+  SelectionSort(N, arr);
+}
+function SelectionSort(N, arr) {
+  for (let i = 0; i < N; i++) {
+    let min = i;
+    for (let j = i + 1; j < N; j++) {
+      if (arr[j] < arr[min]) {
+        min = j;
+      }
+    }
+    [arr[i], arr[min]] = [arr[min], arr[i]];
+  }
+  console.log(arr.join());
+}
+if (process.env.USERNAME === "vasuk") {
+  runProgram(`5
+      5 1 6 3 7`);
+} else {
+  process.stdin.resume();
+  process.stdin.setEncoding("ascii");
+  let read = "";
+  process.stdin.on("data", function (input) {
+    read += input;
+  });
+  process.stdin.on("end", function () {
+    read = read.replace(/\n$/, "");
+    read = read.replace(/\n$/, "");
+    runProgram(read);
+  });
+  process.on("SIGINT", function () {
+    read = read.replace(/\n$/, "");
+    runProgram(read);
+    process.exit(0);
+  });
+}
