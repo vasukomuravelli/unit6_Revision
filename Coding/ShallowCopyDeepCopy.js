c735b2c926cf3c07913ee733cc406b942c97a895
vasukomuravelli <vasukomuravelli96@gmail.com>
2022-02-16 20:44:03 +0530

    done for the day

---
+++ Coding/ShallowCopyDeepCopy.js
@@ -0,0 +1,11 @@
+let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
+
+// let copyArr = JSON.parse(JSON.stringify(arr));
+
+// let copyArr = [...arr];
+
+// let copyArr = Object.assign([], arr);
+
+copyArr[0] = 10;
+
+console.log(copyArr, arr);
