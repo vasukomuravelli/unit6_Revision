613f7df94ae93020e3c1a84e951794ae3f2a96c1
vasukomuravelli <vasukomuravelli96@gmail.com>
2022-02-21 12:43:49 +0530

    folders modified

--- DS/CityTravel.js
+++ DS/CityTravel.js
@@ -1,19 +1,28 @@
-// Link : https://oj.masaischool.com/contest/3132/problem/04
 function runProgram(input) {
-  let N = +input;
-  let dp = new Array(N + 1).fill(-1);
-  dp[0] = 0;
-  dp[1] = 1;
-  console.log(fib(N + 1, dp));
+  input = input.split("\n");
+  let [N, X, K] = input[0].trim().split(" ").map(Number);
+  let arr = input[1]
+    .trim()
+    .split(" ")
+    .map(Number)
+    .sort((a, b) => a - b);
+  CityTravel(N, X, K, arr);
 }
-function fib(N, dp) {
-  if (dp[N - 1] != -1) {
-    return dp[N - 1];
+function CityTravel(N, X, K, arr) {
+  let i = 0;
+  while (K > 0 && i < N - 1) {
+    if (arr[i + 1] - arr[i] <= X) {
+      X -= arr[i + 1] - arr[i];
+    } else {
+      K--;
+    }
+    i++;
   }
-  return (dp[N - 1] = fib(N - 1, dp) + fib(N - 2, dp));
+  console.log(i + 1);
 }
 if (process.env.USERNAME === "vasuk") {
-  runProgram(`5`);
+  runProgram(`3 2 1
+0 2 10`);
 } else {
   process.stdin.resume();
   process.stdin.setEncoding("ascii");
